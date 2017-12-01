d5fc8e47a92754c3f3a290a7570969fbdf3e2621
Danijel-James Wynyard-Geracitano <danijel.james@me.com>
2017-12-01 23:55:34 +1100

    working with voices

--- Dump That Junk/AppDelegate.swift
+++ Dump That Junk/AppDelegate.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2017 Danijel-James Wynyard. All rights reserved.
 //
 
+//  Source code:
+//  http://scriptingosx.com/2016/05/build-an-application-to-run-a-shell-command-in-xcode-and-swift-part-2/
+
 import Cocoa
 
 @NSApplicationMain
@@ -24,19 +27,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var sayThisTextField: NSTextField!
     
-
+    @IBOutlet weak var sayProgress: NSProgressIndicator!
     
     @IBAction func talk(_ sender: NSButton) {
         let path = "/usr/bin/say"
         let textToSay = sayThisTextField.stringValue
-        let arguments = [textToSay]
+        let arguments = [textToSay, "-v", "Alex"]
         
         sender.isEnabled = false
+        sayProgress.startAnimation(self)
         
         let task = Process.launchedProcess(launchPath: path, arguments: arguments)
         task.waitUntilExit()
         
         sender.isEnabled = true
+        sayProgress.stopAnimation(self)
     }
     
 }
